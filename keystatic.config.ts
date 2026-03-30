afc653209722cd4f8769775c5461464b32322c3f
marrywu6 <marrywu6@gmail.com>
2026-03-30 19:25:06 +0800

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -31,6 +31,17 @@ export default config({
             itemLabel: props => props.value
           }
         ),
+        coverImage: fields.image({
+        label: '封面图片',
+        
+        // 1. 物理保存路径：存入与 slug 同名的文件夹下的 content 目录
+        // 星号 (*) 会被 Keystatic 自动替换为当前文章的 slug (如 astro-pages-bilingual)
+        directory: 'src/data/blog/zh/*/content', 
+        
+        // 2. 写入 Markdown 的路径：生成相对路径
+        // 同样使用 * 占位符，生成 ./astro-pages-bilingual/content/ 的前缀
+        publicPath: './*/content/', 
+      }),
         content: fields.markdoc({ 
           label: '正文',
           extension: 'md'
@@ -59,6 +70,17 @@ export default config({
           label: 'Content',
           extension: 'md'
         }),
+        coverImage: fields.image({
+        label: '封面图片',
+        
+        // 1. 物理保存路径：存入与 slug 同名的文件夹下的 content 目录
+        // 星号 (*) 会被 Keystatic 自动替换为当前文章的 slug (如 astro-pages-bilingual)
+        directory: 'src/data/blog/zh/*/content', 
+        
+        // 2. 写入 Markdown 的路径：生成相对路径
+        // 同样使用 * 占位符，生成 ./astro-pages-bilingual/content/ 的前缀
+        publicPath: './*/content/', 
+      }),
       },
     }),
   },
